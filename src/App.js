607a7ca563a29e14c254d0bfc1dbfb00a3701145
DELL <harshrajnit@gmail.com>
2020-05-17 15:22:47 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Join from './Component/Join/Join';
+import Chat from './Component/Chat/Chat';
+
+
+const App=()=>(
+    <Router>
+        <Route path="/" exact component={Join}/>
+        <Route path="/Chat" exact component={Chat}/>
+    </Router>
+    
+);
+export default App;
---
+++ src/Component/TextContainer/TextContainer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css';
+
+const TextContainer = ({ users }) => (
+  <div className="textContainer">
+    {
+      users
+        ? (
+          <div className="activecon">
+            <h1 className="h1class">People currently online:</h1>
+            <div className="activeContainer">
+              <h2>
+                {users.map(({name}) => (
+                  <div key={name} className="activeItem">
+                    
+                <img alt="Online Icon" src={onlineIcon}/>{'  '+name}
+                  </div>
+                ))}
+              </h2>
+            </div>
+          </div>
+        )
+        : null
+    }
+  </div>
+);
+
+export default TextContainer;
