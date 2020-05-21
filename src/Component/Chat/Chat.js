154226d1262d122ad27ef1a18a44a0304d65eae1
DELL <harshrajnit@gmail.com>
2020-05-21 12:32:07 +0530

    Updated server

--- src/Component/Chat/Chat.js
+++ src/Component/Chat/Chat.js
@@ -20,7 +20,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'localhost:5000';
+  const ENDPOINT = 'https://unicorn-chat-app-reactjs.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
