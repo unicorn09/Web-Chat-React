abc5b93b76c46a7df95141f092149e49235bbc47
DELL <harshrajnit@gmail.com>
2020-09-11 14:08:27 +0530

    updatez  index

--- src/Component/Messages/Messages.js
+++ src/Component/Messages/Messages.js
@@ -7,9 +7,12 @@ import Message from './Message/Message';
 import './Messages.css';
 
 const Messages = ({ messages, name }) => (
+  
   <ScrollToBottom className="messages">
+    
     {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
   </ScrollToBottom>
+   
 );
 
 export default Messages;
